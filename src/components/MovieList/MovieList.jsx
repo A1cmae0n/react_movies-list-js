7bc3b8ca222c248f84de5c98a24cf2f76e8a2c51
Your Name <you@example.com>
2025-06-21 14:59:02 +0700

    fixed

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,17 +1,10 @@
 import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
-export const MovieList = ({ movies = [] }) => {
-  // If there are no movies, render nothing
-  if (movies.length === 0) {
-    return null;
-  }
-
-  return (
-    <div className="movie-list">
-      {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
-};
+export const MovieList = ({ movies = [] }) => (
+  <div className="movie-list">
+    {movies.map(movie => (
+      <MovieCard key={movie.id} movie={movie} />
+    ))}
+  </div>
+);
