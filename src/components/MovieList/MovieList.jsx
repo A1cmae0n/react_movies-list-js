39fcfe19e648587f61a0f5b31422ef0455debc8b
Your Name <you@example.com>
2025-06-21 14:50:54 +0700

    first commit

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,17 @@
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies = [] }) => {
+  // If there are no movies, render nothing
+  if (movies.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="movie-list">
+      {movies.map(movie => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+};
